d53cb9226d5b9a53494ced11333085adccec6be1
coldhed <santiago.rdgz2001@gmail.com>
2025-06-03 12:16:42 -0600

    Finished endpoint with bwrap sandboxing

--- app/app.py
+++ app/app.py
@@ -5,10 +5,25 @@ import subprocess
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Hello World! This is the MIPS Compiler API."
+
+"""
+This endpoint compiles a C- program into MIPS assembly code and runs it using the SPIM emulator.
+It expects a JSON payload with the following structure:
+{
+    "program": "C- program as a string",
+    "inputs": ["input1", "input2", ...]  # optional, inputs to be passed to the program
+}
+
+If the number of inputs is not what the program expects, it will default the missing inputs to 0.
+"""
 @app.route('/runCompile', methods = ['POST'])
 def run_compile():
     data = request.get_json()
     program = data.get('program', '')
+    inputs = data.get('inputs', [])
     
     if not program:
         return jsonify({'error' : 'No program provided'}), 400
@@ -16,6 +31,7 @@ def run_compile():
     # make temporary directory for file
     os.makedirs("/tmp/sandbox", exist_ok=True)
     
+    # compile the program 
     try:
         # run the compiler in strict mode so it raises an exception if there is an error
         compiler = Compiler(program, strictMode=True)
@@ -23,13 +39,32 @@ def run_compile():
         
     except Exception as e:
         return jsonify({'error' : 'Error compiling', 'message' : str(e)}), 400
-    
+        
     # run the compiled file through a mips emulator in the terminal
-    result = subprocess.run(
-        ["spim", "-file", "/tmp/sandbox/output.s"],
-        capture_output=True,
-        timeout=15
-    )
+    input_data = '\n'.join(inputs) + '\n'  # join inputs with new lines
+    try:
+        result = subprocess.run(
+            [
+                "bwrap", # creates an unprivileged sandbox environment
+                # mount the necessary directories in the sandbox
+                "--ro-bind", "/usr", "/usr",
+                "--ro-bind", "/lib", "/lib",
+                "--dev", "/dev", # create a dev directory in the sandbox (spim needs it)
+                # bind the sandbox directory to the /tmp/sandbox directory in the sandbox
+                "--bind", "/tmp/sandbox", "/tmp/sandbox", 
+                "--unshare-all", # unshare all namespaces to create an isolated environment
+                "--die-with-parent", # die when the parent process dies
+                "--new-session", # makes it harder for the sandbox process to interfere with the host system
+                "spim", "-file", "/tmp/sandbox/output.s"
+            ],
+            input=input_data.encode(),
+            capture_output=True,
+            timeout=15
+        )
+    except subprocess.TimeoutExpired:
+        return jsonify({'error' : 'Timeout expired while running the compiled file'}), 408
+    except Exception as e:
+        return jsonify({'error' : 'Error running compiled file', 'message' : str(e)}), 500
     
     # if there was an error running the compiled file
     if result.stderr:
@@ -37,7 +72,10 @@ def run_compile():
     
     # return all program outputs as a list, except for first (spim output) and last (empty string after last new line)
     output = result.stdout.decode()
+    
+    # in different environments, the output is different, but what follows 'Loaded' is the actual output
+    output = output[output.find('Loaded'):]  
     return jsonify({'ouptuts' : output.split('\n')[1:-1]}), 200
 
 if __name__ == '__main__':
-	app.run(host='127.0.0.1', port=8000, debug=True)
+	app.run(host='0.0.0.0', port=3001, debug=True)
